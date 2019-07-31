61be1ca8fa4e7853601b2609ca63978ff9cba610
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-31 19:06:56 +0200

    DOne lesson 40

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,13 +1,69 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-router.get("/", (req, res) => {
-  res.send("get user");
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error.");
+  }
 });
 
-router.post("/", (req, res) => {
-  res.send("register user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required.")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email.").isEmail(),
+    check("password", "Please enter a password with 6 or more chars.").isLength(
+      { min: 6 }
+    )
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, password, email } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists." });
+      }
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.send(500).send("Server error.");
+    }
+  }
+);
 
 router.put("/:id", (req, res) => {
   res.send("update user" + req.params.id);
