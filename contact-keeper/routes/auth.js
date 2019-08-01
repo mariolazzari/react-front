6ea652644e1d5e55f1d2c37b9c897b1077ee14ab
Mario Lazzari <mario.lazzari@gmail.com>
2019-08-01 22:16:27 +0200

    Done lesson 42

--- contact-keeper/routes/auth.js
+++ contact-keeper/routes/auth.js
@@ -1,7 +1,65 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../middleware/auth");
 
-router.get("/", (req, res) => {
-  res.send("logged user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error." });
+  }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Please enter a valid email.").isEmail(),
+    check("password", "Password i requied.").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid credential." });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials." });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: "Server error." });
+    }
+  }
+);
 
 module.exports = router;
