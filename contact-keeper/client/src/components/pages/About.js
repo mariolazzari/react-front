92f054f1a62e6212ce9104acc258baf582583bf0
Mario Lazzari <mario.lazzari@gmail.com>
2019-08-02 22:45:48 +0200

    DOne chapter 3

---
+++ contact-keeper/client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className="my-1">Full stach react app</p>
+      <p className="bg-dark p">
+        <strong>1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -1,3 +1,52 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error." });
+  }
+});
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Name is required.")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.status(200).json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "Server error." });
+    }
+  }
+);
 
 module.exports = router;
