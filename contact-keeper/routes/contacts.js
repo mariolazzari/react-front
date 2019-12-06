4c1293065de5581ce775927db37032b50eadb088
Mario Lazzari <mario.lazzari@gmail.com>
2019-12-06 22:29:21 +0100

    Done lesson 49

--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Contact = require("../models/Contact");
 const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 
+// get all contacts
 router.get("/", auth, async (req, res) => {
   try {
     const contacts = await Contact.find({ user: req.user.id }).sort({
@@ -16,6 +16,7 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// add new contact
 router.post(
   "/",
   [
@@ -48,5 +49,62 @@ router.post(
     }
   }
 );
+
+// update contact
+router.put("/:id", auth, async (req, res) => {
+  const { name, mail, phone, site } = req.body;
+
+  // buil contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (mail) contactFields.mail = mail;
+  if (phone) contactFields.phone = phone;
+  if (site) contactFields.ste = site;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: "Contact not found." });
+    }
+
+    // check contact owner
+    if (contact.user.toString() !== req.user.id) {
+      return res, status(401).json({ msg: "User not authorized." });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.status(200).json({ contact });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// delete contact
+router.put("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: "Contact not found." });
+    }
+
+    // check contact owner
+    if (contact.user.toString() !== req.user.id) {
+      return res, status(401).json({ msg: "User not authorized." });
+    }
+
+    await Contact.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ msg: "Contact deleted." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
 
 module.exports = router;
--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -22,9 +22,10 @@ router.post(
       .not()
       .isEmpty(),
     check("email", "Please enter a valid email.").isEmail(),
-    check("password", "Please enter a password with 6 or more chars.").isLength(
-      { min: 6 }
-    )
+    check(
+      "password",
+      "Please enter a password with 6 or more chars."
+    ).isLength({ min: 6 })
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -40,11 +41,15 @@ router.post(
         return res.status(400).json({ msg: "User already exists." });
       }
 
+      // create new user
       user = new User({ name, email, password });
+
+      // encrypt password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
+      // send back jwt token
       const payload = {
         user: {
           id: user.id
